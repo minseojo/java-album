c564f19a01b7718e92994042791c53bb45d4ceb0
minseojo <64322765+jms0531@users.noreply.github.com>
2020-12-04 13:33:02 +0900

    zxc

--- src/image_change.java
+++ src/image_change.java
@@ -1,44 +1,32 @@
 import java.awt.*;
 import java.io.*;
+import java.util.Vector;
 import java.awt.event.*;
 import java.awt.image.*;
-import java.lang.reflect.Member;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Vector;
 
-import javax.imageio.*;
-import javax.print.DocFlavor.URL;
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import javax.xml.transform.Source;
+
 /*
  * 서브 프레임이 안꺼져서 저장할때, 내용이 n*n+1/2개씩 나옴	=> 서브 프로세스 종료 찾기 || 프로그램 최대 사진 2개
  *	추가/	그림판
- *		
 */
 
 public class image_change extends JFrame implements ActionListener {
-	static String buf = "";
+	public String buf = "";
 	char[] arr = new char[100000]; //test.txt 최대 100000글자 까지를 받아옴		
 	String[] copyList = new String[1000];
 	String[] copyName = new String[1000];
 	String[] copyMemo = new String[1000];
-	String[] copyList2 = new String[1000];
-	String[] copyName2 = new String[1000];
-	String[] copyMemo2 = new String[1000];
 	File file = new File("image\\test.txt");	
-	private BufferedImage img; //화면 이미지 출력
 	private JButton button1, button2, button3, button4, button5, button6, button7, button8, btn4, btn5, btn6; //다음 버튼, 이전 버튼, 사진 추가, 사진 삭제, 사진 검색, 편집, 모두보기, 이동, 사진 정렬(8910)
 	private JButton BCbtn1,BCbtn2,BCbtn3,BCbtn4,BCbtn5,BCbtn6,BCbtn7,BCbtn8,BCbtn9,BCbtn10,BCbtn11,BCbtn12;
 	private JPanel imgPanel; // 이미지 나오는 창
 	private int button_index = 0; // 이미지 인덱스(1 ~ 사진 최대 개수, 0은 이미지없음 사진)
 	private int MAX_SIZE = 0, imageCnt = 0; //이미지 최대 개수, 이미지 추가 개수 ()
 	private boolean flag = false, flag2 = false, flag3 = false, flag5 = false, flag6 = false;// (다음, 이전) , (사진 추가, 사진 삭제) , name2,memo2 변수적용
+	private BufferedImage img; //화면 이미지 출력
 	private String imageName; // 추가된 사진 이름
 	private JButton btn = new JButton("저장");
 	private JButton btn2 = new JButton("찾기");
@@ -52,11 +40,11 @@ public class image_change extends JFrame implements ActionListener {
 	private JTextField tfd3 = new JTextField("", 20); // 사진 이름 입력 
 	private JTextField tfd4 = new JTextField("", 20); // 메모 내용 입력
 	String name2="", memo2=""; // test.txt와 폴더안에 실제로 있는 이미지랑 비교해서 실제로 있는 이미지만 벡터에 넣을 name,memo변수
-	JLabel label1 = new JLabel(), label2 = new JLabel(), label3 = new JLabel(), label4 = new JLabel();  // 이미지창에 사진이름, 사진 내용 나오는 라벨, 버튼 인덱스나옴
-	JLabel label5 = new JLabel(), label6 = new JLabel(), label7 = new JLabel(), label8 = new JLabel();
+	JLabel label1 = new JLabel(), label2 = new JLabel(), label3 = new JLabel(), label4 = new JLabel(),label5 = new JLabel();  // 이미지창에 사진이름, 사진 내용 나오는 라벨, 버튼 인덱스나옴
 	JFileChooser fc = new JFileChooser(); //폴더 선택 파일
-	JLabel[] labels = new JLabel[20]; // 
-	JPanel panel = new JPanel(); // 메인 프레임
+	JLabel[] labels = new JLabel[100]; // 
+	JPanel panel = new JPanel(); // 메인 프레임 ,배경색을 바꿀때 공유함
+	
 	public image_change() {
 		mkDir(); // image\\폴더가 없으면 image폴더를 만들고 image\\test.txt 파일을 만든다		
 		fc.setMultiSelectionEnabled(true);
@@ -68,10 +56,13 @@ public class image_change extends JFrame implements ActionListener {
 		showFilesInDIr("image\\");	// "image\\"폴더에 있는 파일들 전부 가져와서 list2에 경로 집어 넣기
 		copyList(); // test.txt에 현재까지 저장된 이미지 경로와 현재 폴더에 실제로 이미지가 있는경우 벡터에 삽입
 		make_mainFrame(); // 메인 프레임 만들기
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i));
+		}
 	}	
 		
 	
-	// image폴더가 없으면 image폴더를 만들고 미리 만들어둔 image_set폴더에 있는 이미지 2개를 복사.
+	// image폴더가 없으면 image폴더를 만듦
 	public void mkDir() {
 		String path = "image\\"; //폴더 경로
 		File Folder = new File(path);
@@ -88,7 +79,6 @@ public class image_change extends JFrame implements ActionListener {
 		    	e.getStackTrace();
 			}        
 	     }
-		  
 	}
 	
 	public void copyText() {	  
@@ -179,16 +169,7 @@ public class image_change extends JFrame implements ActionListener {
 		}
 	    @Override
 	    public void paint(Graphics g) {
-	    	g.drawImage(img, 0, 0, 480, 500, null); //사진, 가로위치, 세로위치, 가로크기, 세로크기, null
-	    }
-	    
-	    @Override
-	    public Dimension getPreferredSize() {
-	    	if (img == null) {
-	    		return new Dimension(480,500);
-	    		} else {
-	    		return new Dimension(img.getWidth(), img.getHeight());
-	        }
+	    	g.drawImage(img, 0, 0, 480, 500, null); //사진, + x, + y, 가로크기, 세로크기, null
 	    }
 	}	
 	
@@ -232,7 +213,7 @@ public class image_change extends JFrame implements ActionListener {
 		        // 파일 선택
 		      	File[] f = fc.getSelectedFiles();		      	
 		        for(File n : f) {
-		        System.out.println("프로그램을 실행시키고 " + imageCnt+1 + "번째 추가한 사진: " + n.getName());
+		        System.out.println("프로그램을 실행시키고 " + imageCnt + "번째 추가한 사진: " + n.getName());
 		        imageName = n.getName();		         //이미지 파일 집어넣기(img경로, 복사할 img이름)
 		        }
 		       
@@ -285,11 +266,11 @@ public class image_change extends JFrame implements ActionListener {
 		}
 		//사진 목록
 		if(e.getSource() == button6)  {
-			make_subFrame4(); //btn3로 작동
+			make_subFrame4(); //button로 작동
 		}
 		//사진 편집
 		if(e.getSource() == button7) {	
-			make_subFrame5(); // button 9,10,11로 작동
+			make_subFrame5(); // btn(6,5,4)로 작동
 		}	
 		//페이지 이동
 		if(e.getSource() == button8 && MAX_SIZE > 1)  {
@@ -301,8 +282,6 @@ public class image_change extends JFrame implements ActionListener {
 			flag3 = true;
 			name.add(tfd.getText());
 			memo.add(tfd2.getText());
-			tfd.setText("");
-			tfd2.setText("");
 			System.out.println("저장한 사진 경로: " + "image\\" + imageName);
 			System.out.println("저장한 사진 이름: " + name.get(MAX_SIZE - 1).toString());
 			System.out.println("저장한 사진 내용: " + memo.get(MAX_SIZE - 1).toString());
@@ -313,13 +292,17 @@ public class image_change extends JFrame implements ActionListener {
 
 			try {
 				FileWriter fileWriter = new FileWriter(filePath,true);
+				if(!buf.contains(tfd.getText())) // test.txt중복체크
 				fileWriter.write("image\\" + imageName + ";" + name.get(button_index) + ":" + memo.get(button_index) + "|"); // |로 구분자를 정함				   
 				fileWriter.close();
 			  } catch (IOException e1) {
 				  e1.printStackTrace();
 			  }
+			tfd.setText("");
+			tfd2.setText("");
 		}
 		
+		//사진 이름으로 찾기
 		if(e.getSource() == btn2) {
 			int cnt=0;
 			String s = tfd3.getText();
@@ -337,6 +320,8 @@ public class image_change extends JFrame implements ActionListener {
 				System.out.println("찾는 사진이 없습니다.");
 			}
 		}
+		
+		// 페이지 이동
 		if(e.getSource() == btn3) {
 			int cnt=0;
 			int s = Integer.parseInt(tfd4.getText());
@@ -440,6 +425,7 @@ public class image_change extends JFrame implements ActionListener {
             e.printStackTrace();
         }
 	}
+	
 	//메인 프레임 만들기
 	public void make_mainFrame() {
 		imgPanel = new ChangeImagePanel();
@@ -578,33 +564,6 @@ public class image_change extends JFrame implements ActionListener {
 		
 		pack();
 	}
-		
-	/* 초기 앨범 기본 파일 복사
-	public void copyFile(String path, String imageName) {
-        //복사될 파일경로
-        String copyFilePath = "image\\" + imageName;
-        //복사파일객체생성
-        File copyFile = new File(copyFilePath);
-        System.out.println(copyFilePath + "사진을 복사했습니다.");
-        try {           
-            FileInputStream fis = new FileInputStream("image_set\\" + imageName); //저장해둔 image_set 에서 앨범 기본이미지를 가져옴.
-            FileOutputStream fos = new FileOutputStream(copyFile); //복사할파일
-            
-            int fileByte = 0; 
-            // fis.read()가 -1 이면 파일을 다 읽은것
-            while((fileByte = fis.read()) != -1) {
-                fos.write(fileByte);
-            }
-            //자원사용종료
-            fis.close();
-            fos.close();
-            
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }		
-	}*/
 	
 	//사진 저장 창
 	public void make_subFrame() {
@@ -804,7 +763,7 @@ public class image_change extends JFrame implements ActionListener {
 		
 		btn4 = new JButton("앨범 배경색 바꾸기");
 		btn5 = new JButton("내용 보기");
-		btn6 = new JButton("사진 최신/오래된순 보기");
+		btn6 = new JButton("사진 순서 반대로 바꾸기");
 		
 		btn4.setBorderPainted(false);	
 		btn5.setBorderPainted(false);
@@ -1054,40 +1013,4 @@ https://calsifer.tistory.com/239
 https://programmingsummaries.tistory.com/61    GUI 구현
 https://raccoonjy.tistory.com/17
 https://halfmoon9.tistory.com/12
-
-for(int i=0; i<1000; i++) { // 0은 |로 초기화 했으므로 1부터 시작
-			if(arr[i] == '|') { // 만약 구분자 나오면
-				s = ""; // 초기화
-				s2 = "";
-				s3 = ""; // 초기화
-				flag5 = false;
-				flag6 = false; // 초기화
-				//MAX_SIZE++;
-				for(int j=start; j<i; j++) {
-					if(arr[j] == ';') {
-						s2="";
-						flag5=true;
-						j++;
-					}
-					if(arr[j] == ':') { // :전에는 사진경로, :이후에는 사진 내용
-						s3="";    
-						flag6=true;
-						j++;
-					}
-					if(flag5 == false) { // 사진 경로
-						s+=arr[j];
-					}
-					if(flag5==true && flag6 == false) {
-						s2+=arr[j];
-					}
-					s3+=arr[j];
-				}
-				if(cnt!=0) // 파일을 처음부터 읽어야 컴파일이 되는데, 처음 내용은 null이므로 넘어감
-					System.out.println(cnt + ". "+ s2); // 사진 이름 출력
-					//name.add(s2);  // 사진 이름 저장
-					//memo.add(s3); //사진 내용 저장
-					start=i+1; // 다음 조건이 성립할때의 시작값은 이전 조건이 끝나고 다음 인덱스값
-					cnt++; // 현재 저장된 총 사진 개수
-			}
-		}
 */
